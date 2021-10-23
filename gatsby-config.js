438a0ed0f212c2cc3dae26b4008e2800e9e7e1c4
Benedikt Schöffmann <benedikt.schoeffmann@gmail.com>
2021-10-23 12:59:28 +0200

    Reformat config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,20 +3,22 @@ module.exports = {
     title: "Melanie R. Silbernagl",
     description: "Melanie R. Silbernagl - Mixed Media Artist / Art therapist",
     siteLanguage: "de-at",
-    navigation: [
-      {
-        title: "Home",
-        slug: "/"
-      },
-      {
-        title: "Blog",
-        slug: "/blog"
-      },
-      {
-        title: "About",
-        slug: "/about"
-      }
-    ]
+    navigation: {
+      nodes: [
+        {
+          title: "Home",
+          slug: "/"
+        },
+        {
+          title: "Blog",
+          slug: "/blog"
+        },
+        {
+          title: "About",
+          slug: "/about"
+        }
+      ]
+    }
   },
   plugins: [
     "gatsby-plugin-image",
