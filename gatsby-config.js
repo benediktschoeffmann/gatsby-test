8b83841ce1bae7ee3f1314db430ee9f768cd6d80
Benedikt Schöffmann <benedikt.schoeffmann@gmail.com>
2021-10-23 10:23:21 +0200

    Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,17 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "test",
+    title: "My Super Cool Blog",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: `blog`,
-        path: `${__dirname}/blog`,
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `posts`,
-        path: `${__dirname}/src/posts/`,
+        name: `blog`,
+        path: `${__dirname}/blog/`,
       },
     },
-    {
-      resolve: "gatsby-plugin-page-creator",
-      options: {
-        path: `${__dirname}/src/posts`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        defaultLayouts: {
-          posts: require.resolve("./src/components/posts-layout.js"),
-          default: require.resolve("./src/components/default-page-layout.js"),
-        },
-      },
-    },
-  ]
+    "gatsby-plugin-mdx",
+  ],
 };
