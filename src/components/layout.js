64be3f1664afe3d582075cd33514bd016e7aacc7
Benedikt Schöffmann <benedikt.schoeffmann@gmail.com>
2021-10-23 13:00:20 +0200

    Use Nav component

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,13 +5,14 @@ import {
   heading
 } from './layout.module.css'
 import Head from './head.js'
+import Nav from './nav.js'
 
 const Layout = ({ pageTitle, children }) => {
 
   return (
     <div className={container}>
       <Head pageTitle={pageTitle}/>
-      
+      <Nav/>
       <main>
         <h1 className={heading}>{pageTitle}</h1>
         {children}
